a35bc5135d8592645d84a0f2805a12941cf4e9e1
wnghks517 <wnghks517@gmail.com>
2023-08-11 19:15:44 +0900

    servlet JSP를 이용하여 회원가입 조회 등

--- src/main/java/hello/servlet/basic/doamin/MemberRepository.java
+++ src/main/java/hello/servlet/basic/doamin/MemberRepository.java
@@ -1,28 +1,35 @@
 package hello.servlet.basic.doamin;
 
-import org.springframework.stereotype.Repository;
+import hello.servlet.basic.doamin.Member;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
-
-/*
-* 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용고려
-* */
+/**
+ * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+ */
 public class MemberRepository {
-    private static Map<Long, Member> store = new HashMap<>();
-    private static long sequence = 0L;
-
-    //싱글톤
+    private static Map<Long, Member> store = new HashMap<>(); //static 사용
+    private static long sequence = 0L; //static 사용
     private static final MemberRepository instance = new MemberRepository();
-
     public static MemberRepository getInstance() {
         return instance;
     }
-
+    private MemberRepository() {
+    }
     public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(), member);
         return member;
+    }
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+    public void clearStore() {
+        store.clear();
     }
 }
